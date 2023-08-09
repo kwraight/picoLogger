be069919c5d948daa9d311c3dc79f5879a8be13c
Kenneth Gibb Wraight <kenneth.gibb.wraight@cern.ch>
2023-08-09 10:44:23 +0100

    template for connection file

---
+++ notebooks/connectionDetails_template.py
@@ -0,0 +1,9 @@
+### useful details
+
+def GetInfluxCredentials():
+    print("Getting Influx info.")
+    return {'bucket':"BUCKET",
+            'org':"ORG",
+            'token':"TOKEN",
+            'url':"URL:PORT"
+            }
